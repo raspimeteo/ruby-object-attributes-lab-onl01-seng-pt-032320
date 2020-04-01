8a08534e8b110964b1dfbf26aa98f1fdda490de3
John Zonneveld <jjczon@gmail.com>
2020-04-01 16:42:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,12 @@
 class Person
   def name=(person_name)
-    @name=person_name
+    @name = person_name
   end
   def name
     @name
   end
   def job=(person_job)
-    @job=person_job
+    @job = person_job
   end
   def job
     @job
